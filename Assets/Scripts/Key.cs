0702996a33c6ff2579e8a46c5d87a50ddbf8de19
K1aymore <klaymorer@protonmail.com>
2025-11-25 12:16:54 -0800

    Beginning key collision handling

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,16 +5,34 @@ public class Key : MonoBehaviour
 {
     public char Char;
     public TextMeshPro Text;
+    public TextMeshPro Screen;
+    
+    bool colliding = false;
+    Vector3 StartPos;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Text.text = "" + Char; // convert to string
+        StartPos = gameObject.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (colliding) {
+            gameObject.transform.localPosition = StartPos + new Vector3(0f, -0.2f, 0f);
+        }
+        else {
+            gameObject.transform.localPosition = StartPos;
+        }
+    }
+    
+    void OnTriggerEnter() {
+        colliding = true;
+        Screen.text += Char;
+    }
+    void OnTriggerExit() {
+        colliding = false;
     }
 }
--- Assets/Scripts/Keyboard.cs
+++ Assets/Scripts/Keyboard.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class Keyboard : MonoBehaviour
 {
     public GameObject keyPrefab;
+    public TextMeshPro Screen;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +14,7 @@ public class Keyboard : MonoBehaviour
         for (int i = 0; i < keyChars.Length; i++) {
             o = Instantiate(keyPrefab, new Vector3(2.5f - (i%10)*0.4f, 0, -4.1f+(i/10)*0.4f), Quaternion.identity);
             o.GetComponent<Key>().Char = keyChars[i];
+            o.GetComponent<Key>().Screen = Screen;
         }
         
     }
