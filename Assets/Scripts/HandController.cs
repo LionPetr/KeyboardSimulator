99d603bf8f1e8afcbb183ad14d530420303cd3a6
lion <lionpetrenko@gmail.com>
2025-12-05 14:21:28 -0800

    added movement for arm so the hand can be adjusted within bounds so that all keys are reachable easily

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -5,6 +5,10 @@ public class HandController : MonoBehaviour
     public SceneNode selectedObject;
 
     private float rotationSpeed = 90f;
+    private float movementSpeed = 2f;
+
+    Vector3 armMovement = Vector3.zero;
+    
     void Start()
     {
         
@@ -15,16 +19,40 @@ public class HandController : MonoBehaviour
         float yaw = 0f;
         float pitch = 0f;
         float yAxisRot = 0f;
+        armMovement = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.A))
-            yaw -= rotationSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.D))
-            yaw += rotationSpeed * Time.deltaTime;
+        if (!selectedObject.CompareTag("Arm"))
+        {
+            if (Input.GetKey(KeyCode.A))
+                yaw -= rotationSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D))
+                yaw += rotationSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.W))
-            pitch -= rotationSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.S))
-            pitch += rotationSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W))
+                pitch -= rotationSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.S))
+                pitch += rotationSpeed * Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A))
+                armMovement.x += movementSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D))
+                armMovement.x -= movementSpeed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.W))
+                armMovement.z -= movementSpeed * Time.deltaTime;
+            if (Input.GetKey(KeyCode.S))
+                armMovement.z += movementSpeed * Time.deltaTime;
+
+            selectedObject.transform.position += armMovement;
+            
+            Vector3 pos = selectedObject.transform.position;
+            pos.x = Mathf.Clamp(pos.x, -3.0f, 3.0f);
+            pos.z = Mathf.Clamp(pos.z, -1.0f, 1.0f);
+            selectedObject.transform.position = pos;
+        }
+
 
         //for rotating the hand itself in case the person messes it up bad
         if (selectedObject.name == "hand")
